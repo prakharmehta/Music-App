e77dbb80f5112c4182c1e43ea6fda68509deca18
prakharnmehta <prakh.mehta@gmail.com>
2020-04-23 21:23:31 +0530

    Added home page design

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('./views'))
+
+app.get('/', (req, res) => {
+    res.render('music')
+})
+
 console.log('Server is up');
 
 app.listen(3000);
